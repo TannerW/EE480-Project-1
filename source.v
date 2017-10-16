ef13a5637a33b998779019eac220d291348e7b2a
cjva226 <32625248+cjva226@users.noreply.github.com>
2017-10-16 17:33:30 -0400

    Update source.v

--- source.v
+++ source.v
@@ -44,8 +44,21 @@ endmodule
 
 //--------------------regfile------------------------
 module regfile();
-
+	input clk;
+	input `word in;
+	input [3:0] regc, regsel;
+	output `word out;
+	reg `word register `regsize;
 	
+	always@ (posedge clk)begin
+		case(regc)
+		0: register[regsel] <= register[regsel];
+		1: register[regsel] <= in;
+		2: register[regsel] <= register[regsel] << 1;
+		3:register[regsel] <= register[regsel] >> 1;
+	endcase
+	end
+	assign out = register[regsel];
 	
 endmodule
 
@@ -79,78 +92,33 @@ module oracle();
 endmodule
 
 //-----------------------------------------------------main mem--------------------------------
-module mainMem();
-
+module mainMem(clk, write, datain, dataout, addr);
+	input clk, write;
+	input `word datain, addr;
+	output `word dataout;
+	
+	reg `word memory `memsize;
+	
+	always@ (posedge clk)begin
+		if (write) memory[addr] <= datain;
+		dataout <= memory[addr];
+	end
+	
 endmodule
 
 //-----------------------------------------------------lss_reg---------------------------------
-//module nbit_lss_struct #(parameter n = 4) (clk, in, lssc, arithnolog, set, clr, out);
-//
-//	input 				clk, set, clr,arithnolog;
-//	input 	[n-1:0] 	in;
-//	input 	[1:0] 		lssc;
-//	output	[n-1:0] 	out;
-//	logic 				left_end, right_end;
-//
-//	assign left_end = arithnolog ? out[n-1]:0;
-//	assign right_end = arithnolog ? out[0]:0;
-//	
-//	//mux2x1_rtl ({})
-//	
-//	genvar i;
-//	generate
-//		for (i = 1; i != n-1; i = i + 1)
-//		begin: b
-//			lss_bitslice_struct lss_reg(clk, out[i-1], out[i+1], in[i], lssc, set, clr, out[i]);
-//		end
-//	endgenerate 
-//	
-//	lss_bitslice_struct b0(clk, left_end, out[1], in[0], lssc, set, clr, out[0]);
-//	lss_bitslice_struct bn(clk, out[n-2], right_end, in[n-1], lssc, set, clr, out[n-1]);
-//	
-//
-//endmodule
-//
-//module mux4x1_rtl(in, sel, out);
-//
-//    input	[3:0] 	in;
-//    input	[1:0] 	sel;
-//    output 			out;
-//    
-//    assign out = sel[1] ? (sel[0] ? in[3]:in[2]) : (sel[0] ? in[1]:in[0]);
-//    
-//    
-//endmodule
-//
-//
-//module lss_bitslice_struct(clk, sl_in, sr_in, in, lssc, set, clr, out);
-//
-//	input			clk, in, sl_in, sr_in, set, clr;
-//	input	[1:0] 	lssc;
-//	output			out;
-//	
-//	wire mux_o, dff_o, dff_o_cmp;
-//	
-//	mux4x1_rtl funcSel({sr_in, dff_o, in, sl_in}, lssc, mux_o);
-//	
-//	dff_beh dff(clk, mux_o, set, clr, dff_o, dff_o_cmp);
-//	
-//	assign out = dff_o;
-//
-//endmodule
-//
-//module dff_beh(clk, d, set, clr, q, q_not);
-//
-//	input	clk, d, set, clr;
-//	output	q, q_not;
-//	
-//	assign q_not = ~q;
-//	
-//	always@ (posedge clk) begin
-//		if(set == 1)		q <= 1;
-//		else if(clr == 1) 	q <= 0;
-//		else 				q <= d;
-//	end
-//
-//endmodule
 
+module register(clk, out, in, control)
+	input clk;
+	input [1:0] control;
+	input `word in;
+	output `word out;
+	
+	always@ (posedge clk)begin
+		case(control)
+		0: out <= out;
+		1: out <= in;
+		2: out <= out << 1;
+		3: out <= out >> 1;
+	end
+endmodule
